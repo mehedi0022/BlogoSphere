6c2036251b1958629dc1234315e0f92b7051c88f
MD Mehedi Hassan <mdmehedihassan0022@gmail.com>
2024-12-09 16:00:25 +0600

    Update AdminContext.jsx

--- Frontend/src/context/AdminContext.jsx
+++ Frontend/src/context/AdminContext.jsx
@@ -6,7 +6,7 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
-  const backendURL = "http://localhost:5500";
+  const backendURL = "https://blog-sand-two-72.vercel.app";
 
   const value = {
     aToken,
